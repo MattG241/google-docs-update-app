f48ee1c0146bc66041fd9b3c86fd9807b73dcaac
Matthew <matthew@ryderwear.com>
2024-09-25 13:11:02 +0930

    Changes

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
-import google.auth
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
-import os
+
 app = Flask(__name__)
 
 # Add your Google Sheets API credentials here
 SPREADSHEET_ID = '1dZ-iYwWfQjAsTFknw9nhI8nocV7fAdzE8NbSiSzRyy0'
+CREDENTIALS_FILE = '/Users/tijanamatias/Desktop/google-docs-update-app/credentials.json'  # Path to your credentials file
 
 def find_first_empty_row(service):
     range_name = 'WH to CS OOS Comms!A:A'  # Ensure this matches your sheet name
@@ -20,12 +21,12 @@ def index():
         sku = request.form['sku']
 
         # Authenticate and build the Google Sheets service
-        creds, _ = google.auth.default()
+        creds = service_account.Credentials.from_service_account_file(CREDENTIALS_FILE)
         service = build('sheets', 'v4', credentials=creds)
 
         # Find the first empty row and update it
         first_empty_row = find_first_empty_row(service)
-        range_to_update = f'WH to CS OOS Comms!A{first_empty_row}:B{first_empty_row}'  # Update the range to include columns A and B
+        range_to_update = f'WH to CS OOS Comms!A{first_empty_row}:B{first_empty_row}'
 
         values = [[order_number, sku]]
         body = {'values': values}
@@ -37,5 +38,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Run the app on the specified host and port
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)

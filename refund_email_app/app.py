21ae61f70fadfaf9ba1c0a5ec7f1f5c8fc2b022f
Matthew <matthew@ryderwear.com>
2024-09-25 11:22:40 +0930

    Use Heroku port

--- refund_email_app/app.py
+++ refund_email_app/app.py
@@ -8,4 +8,6 @@ def home():
     return "Refund and Email App"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    # Use the PORT environment variable or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
